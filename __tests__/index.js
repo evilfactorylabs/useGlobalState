9f14cc4f4451b5d2bbdfa6c9e90921cd9e24dd0b
Rin <ri7nz.labs@gmail.com>
2019-07-29 09:33:35 +0700

    added: minimum test (#8)

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,2 +1,60 @@
-    test.todo('StateProvider')
-    test.todo('useGlobalState')
+import React from 'react'
+import {useGlobalState, StateProvider} from '../lib'
+import {fireEvent, render} from '@testing-library/react'
+
+const Provider  = jest.fn(StateProvider)
+
+test('StateProvider & useGlobalState test', () => {
+
+    const Props ={
+        reducer: (state, action) => {
+            switch(action.type){
+                case 'TEST': return {
+                    ...state,
+                    test: true 
+                } 
+                default: return state
+            } 
+        },
+        initialState: {test: false}
+    } 
+
+    const ShowTest = () => {
+        const [{test}] = useGlobalState()
+        return (
+            <span data-testid="showTest">
+                {JSON.stringify(test)}
+            </span>
+        )
+    } 
+
+    const ChangeTest = () => {
+        const [, dispatch] = useGlobalState()
+
+        const handleClick = () => {
+            dispatch({
+                type: 'TEST'
+            }) 
+        }
+
+        return (<button data-testid="changeTest" onClick={handleClick}>Change Test</button>)
+    }
+
+    const {container, getByTestId } = render(
+        <Provider {...Props}>
+            <ShowTest/>
+            <ChangeTest/>
+        </Provider>
+    )
+
+    const showTest = getByTestId('showTest')
+    const changeTest = getByTestId('changeTest')
+
+    expect(Provider).toHaveBeenCalled()
+    expect(showTest).toHaveTextContent('false')
+    expect(container).toBeInTheDocument()
+
+    fireEvent.click(changeTest)
+    expect(showTest).toHaveTextContent('true')
+
+})
